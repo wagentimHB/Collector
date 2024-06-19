580543da7a2f28792c72215342cf12b7291b9a83
wagentim <bhwerbung@gmail.com>
2024-06-19 22:24:10 +0200

    initial project

---
+++ collector/src/main/java/de/wagentim/App.java
@@ -0,0 +1,13 @@
+package de.wagentim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
